9cf925ee0e7b2739670313e809c97f97ce78e73d
Keannu Bernasol <noehbernasol0@gmail.com>
2026-08-02 21:02:34 +0800

    Remove redundant code block

--- api/auxos/repo/inverter.py
+++ api/auxos/repo/inverter.py
@@ -10,9 +10,6 @@ logger = logging.getLogger(__name__)
 class Inverter(Repo):
     """Inverter Actions"""
 
-    def __init__(self, CONFIG):
-        self.CONFIG = CONFIG
-
     @retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=2, max=15))
     def get_inverter(self):
         url = f"{self.CONFIG.AUXSOL_BASE_URL}/archive/inverter/getPlantByInverterSN/{self.CONFIG.AUXSOL_INVERTER_SN}"
